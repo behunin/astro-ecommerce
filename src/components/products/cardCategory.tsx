cae50bcd1f293dafcef5d4f5363bfb7c35377b2a
Behunin <l3ehunin@gmail.com>
2023-04-11 12:18:18 -0600

    cardCategory: fix width on mobile

--- src/components/products/cardCategory.tsx
+++ src/components/products/cardCategory.tsx
@@ -12,7 +12,7 @@ export default function CardProduct({
 }: Props) {
 
   return (
-    <a href="#" class="basis-60">
+    <a href="#" class="basis-60 w-4/5 md:w-unset">
       <div class={`outline-1 rounded grid grid-flow-row items-center mb-4 lg:mb-0 ${classList}`}
         style={{ "background-image": `url(${thumb_src})`, "background-size": 'cover', "background-color": "gray", "background-blend-mode": "multiply" }}>
         <div class="text-center">
